d534828123720ad35bca4ae5bf026af2765a5553
mruczy <mru.michal@gazeta.pl>
2023-01-17 15:04:24 +0100

    add Ognisko object

---
+++ src/Ognisko.h
@@ -0,0 +1,23 @@
+#ifndef OGNISKO_H
+#define OGNISKO_H
+
+#include "ofMain.h"
+#include "v3.h"
+#include "Object.h"
+
+
+class Ognisko : public Object
+{
+public:
+	ofMaterial materiala;
+	ofMaterial materialb;
+	ofConePrimitive cone;
+	std::vector<ofIcoSpherePrimitive*> sphere;
+
+	Ognisko(v3 posvec);
+	virtual ~Ognisko();
+
+	void draw();
+};
+
+#endif
