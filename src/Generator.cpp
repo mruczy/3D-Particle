8b91b1a64d4400789b5b114306ad7c0dd9c92ca8
mruczy <mru.michal@gazeta.pl>
2023-01-21 03:36:18 +0100

    add variable for particle generating

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -1,15 +1,23 @@
 #include "Generator.h"
 #include <random>
 
-Generator::Generator(v3 posvec, int amount)
+Generator::Generator(v3 posvec, v3 velvec, float g, int amount, float radius, float mass, int life, int resolution, ofColor_<unsigned char> _color)
 {
 	this->setPosition(posvec);
+	this->setVelocity(velvec);
+	this->setG(g);
 	this->setAmount(amount);
+	this->setRadius(radius);
+	this->setMass(mass);
+	this->setLife(life);
+	this->setResolution(resolution);
+
 	this->sphere.setPosition(this->getPosition().x, this->getPosition().y, this->getPosition().z);
 	this->material.setDiffuseColor(ofFloatColor::darkGreen);
 
 	this->sphere.setRadius(10);
 	this->sphere.setResolution(2);
+	this->color.set(_color);
 }
 
 Generator::~Generator()
@@ -26,6 +34,26 @@ void Generator::setPosition(v3 posvec)
 	this->pos = posvec;
 }
 
+v3 Generator::getVelocity()
+{
+	return vel;
+}
+
+void Generator::setVelocity(v3 velvec)
+{
+	this->vel = velvec;
+}
+
+float Generator::getG()
+{
+	return g;
+}
+
+void Generator::setG(float _g)
+{
+	this->g = _g;
+}
+
 int Generator::getAmount()
 {
 	return amount;
@@ -36,11 +64,56 @@ void Generator::setAmount(int newAmount)
 	this->amount = newAmount;
 }
 
-void Generator::generate(vector<Particle*>* particle)
+float Generator::getRadius()
+{
+	return radius;
+}
+
+void Generator::setRadius(float _radius)
+{
+	this->radius = _radius;
+}
+
+float Generator::getMass()
+{
+	return mass;
+}
+
+void Generator::setMass(float _mass)
+{
+	this->mass = _mass;
+}
+
+int Generator::getLife()
+{
+	return life;
+}
+
+void Generator::setLife(int _life)
+{
+	this->life = _life;
+}
+
+int Generator::getResolution()
+{
+	return resolution;
+}
+
+void Generator::setResolution(int _resolution)
+{
+	this->resolution = _resolution;
+}
+
+ofColor_<unsigned char> Generator::getColor()
+{
+	return this->color;
+}
+
+void Generator::generate(vector<Particle*>* particle, int rd)
 {
 	for (int i = 0; i < amount;i++)
 	{
-		particle->push_back(new Particle(v3(this->getPosition().x + random(-500, 500), this->getPosition().y, this->getPosition().z + random(-500, 500)), v3(random(10.0, 10.5), 3, random(10.0, 10.5)), 10.0, 1.0, 200));
+		particle->push_back(new Particle(v3(this->getPosition().x + random(-rd, rd), this->getPosition().y, this->getPosition().z + random(-rd, rd)), this->getVelocity(), getRadius(), getMass(), getG(), getLife(), getResolution(), getColor()));
 	}
 }
 
--- src/Generator.h
+++ src/Generator.h
@@ -7,25 +7,50 @@
 class Generator
 {
 private:
-	v3 pos;
+	v3 pos;									
+	v3 vel;									//particle start velocity
+	float g;								//g force
 	int amount;								//how many particle generate in one tick
 	ofMaterial material;
 	ofIcoSpherePrimitive sphere;
 
+	float radius;							
+	float mass;								
+	int life;								
+	int resolution;							
+	ofColor_<unsigned char> color;			
+
 public:
-	Generator(v3 posvec, int amount);
+	Generator(v3 posvec, v3 fvec, float g, int amount, float radius, float mass, int life, int resolution, ofColor_<unsigned char> color);
 	virtual ~Generator();
 
-	//get pos from emiter
-	//in future get pos of primitive//
 	v3 getPosition();
 	void setPosition(v3 posvec);
+
+	v3 getVelocity();
+	void setVelocity(v3 fvec);
+
+	float getG();
+	void setG(float _g);
 
 	int getAmount();
 	void setAmount(int newAmount);
 
-	//Particle generator 
-	void generate(vector<Particle*> *particle);
+	float getRadius();
+	void setRadius(float _radius);
+	
+	float getMass();
+	void setMass(float _mass);
+
+	int getLife();
+	void setLife(int _life);
+
+	int getResolution();
+	void setResolution(int _resolution);
+
+	ofColor_<unsigned char> getColor();
+
+	void generate(vector<Particle*>* particle, int rd);
 
 	float random(float min, float max);
 
